054bd4422d7b3fb400686545519446e312dd7878
andrewbrayn99 <andrewbrayn99@gmail.com>
2022-04-04 09:15:02 +0700

    update routing

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import ThemeDecorator from '@enact/sandstone/ThemeDecorator';
 import Setting from '../Views/Setting'
 import { Route, Routable, Link } from '@enact/ui/Routable'
 import Home from '../Views/Home/Home.jsx'
+import VideoPlayer from '../Views/Detail';
 // import View from '../Views'
 
 const Views = Routable({ navigate: 'onNavigate' }, ({ children }) => <div>{children}</div>);
@@ -13,11 +14,13 @@ function App(props) {
 	const handleNavigate = useCallback((ev) => nav(ev.path), [nav]);
 	return (
 		<div {...props}>
-			<Views path={path} onNavigate={handleNavigate}>
+			{/* <Views path={path} onNavigate={handleNavigate}>
 				<Route path="main" component={Home}>
 					<Route path='setting' component={Setting} />
 				</Route>
-			</Views>
+			</Views> */}
+			<VideoPlayer />
+			{/* <Setting /> */}
 		</div>
 	)
 }
