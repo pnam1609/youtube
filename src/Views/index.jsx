353f51be0ea35d5214db5950b0335d80daf1db00
andrewbrayn99 <andrewbrayn99@gmail.com>
2022-04-04 09:33:27 +0700

    dang con loi

--- src/Views/index.jsx
+++ src/Views/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import SideBar from '../Components/SideBar'
 import Topbar from '../Components/TopBar'
+import RowList from './Home/RowList/RowList'
+import { Row, Cell, Column } from '@enact/ui/Layout'
 
 function Views() {
-  return (
-    <div>
-      <SideBar />
-      <Topbar />
-    </div>
-  )
+    return (
+        <Row style={{ height: '100%' }}>
+            <Cell size="20%"><SideBar /></Cell>
+            <Cell>
+                <Column>
+                    <RowList />
+                </Column>
+            </Cell>
+        </Row>
+    )
 }
 
 export default Views
