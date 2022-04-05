7718dcb2d448566a14d8696bcb891efd90c51869
Nguyentanduy <duynguyen251000@gmail.com>
2022-04-05 08:27:32 +0700

    add data api

--- src/Views/Home/RowList/RowList.jsx
+++ src/Views/Home/RowList/RowList.jsx
@@ -1,29 +1,36 @@
 import RowListItem from "./RowListItem/RowListItem"
 import Scroller from '@enact/sandstone/Scroller';
+import axios from 'axios';
+import { useEffect, useState } from "react";
+
 const RowList = (props) => {
-    const data = [1, 2, 3, 4]
-    return <div {...props}>
-            <Scroller horizontalScrollbar="hidden">
+    const [data, setData] = useState([])
+    useEffect(() => {
+        axios.get("https://tue0305.github.io/recipes.json").then(res => {
+            const keys = Object.keys(res.data);
+            const map = new Map();
+            keys.forEach(key => {
+                map.set(key, res.data[key])
+            })
+            setData(Array.from(map.values()))
+        }
+        ).catch(err => {
+            console.log(err)
+        })
+    }, [])
+    console.log(data);
+    return <>
+        <div {...props}>
+            {data && data?.map((items, index) => (<Scroller key={index} horizontalScrollbar="hidden">
                 <div style={{ display: "flex" }}>
-                    {data.map((item, index) => {
-                        return <RowListItem key={index}/>
+                    {items.map((item, index) => {
+                        return <RowListItem key={index} title={item.title} thumbnail={item.thumbnail} longDescription={item.longDescription} />
                     })}
                 </div>
-            </Scroller>
-            <Scroller horizontalScrollbar="hidden">
-                <div style={{ display: "flex" }}>
-                    {data.map((item, index) => {
-                        return <RowListItem key={index} />
-                    })}
-                </div>
-            </Scroller>
-            <Scroller horizontalScrollbar="hidden">
-                <div style={{ display: "flex" }}>
-                    {data.map((item, index) => {
-                        return <RowListItem key={index} />
-                    })}
-                </div>
-            </Scroller>
-    </div>
+            </Scroller>))}
+        </div>
+    </>
+
+
 }
 export default RowList
--- src/Views/Home/RowList/RowListItem/RowListItem.jsx
+++ src/Views/Home/RowList/RowListItem/RowListItem.jsx
@@ -1,21 +1,14 @@
-import Item from '@enact/sandstone/Item';
 import ImageItem from '@enact/sandstone/ImageItem';
-import kind from '@enact/core/kind';
 
-const SelectionComponent = kind({
-  render: () => <div>custom selection component</div>
-});
 const RowListItem = (props) => {
+
   return <div {...props}>
-
     <ImageItem
-      selectionComponent={SelectionComponent}
-      style={{ width: "500px", height: "500px"}}
-      src="https://i.ytimg.com/vi/hA_mbqIPNuA/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDYE8yiNLeZRjzjT2gjiWAToOPzhw"
-      label="Music"
+      style={{ width: "500px", height: "500px" }}
+      src={props.thumbnail}
+      label={props.title}
     >
-      horizontalScrollbar horizontalScrollbar horizontalScrollbar horizontalScrollbar
-
+      {props.longDescription}
     </ImageItem>
 
   </div>
