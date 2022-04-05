6b2711c2127c7e03788f1f67d23234dc14d2df79
andrewbrayn99 <andrewbrayn99@gmail.com>
2022-04-05 09:13:30 +0700

    update rowlist

--- src/Views/Home/RowList/RowList.jsx
+++ src/Views/Home/RowList/RowList.jsx
@@ -2,43 +2,39 @@ import RowListItem from "./RowListItem/RowListItem"
 import Scroller from '@enact/sandstone/Scroller';
 import axios from 'axios';
 import { useEffect, useState } from "react";
+import React from 'react'
 
-const RowList = (props) => {
-    const [data, setData] = useState([])
+const RowList = React.memo((props) => {
+    const [data, setData] = useState({})
     useEffect(() => {
-        axios.get("https://tue0305.github.io/recipes.json").then(res => {
-            const keys = Object.keys(res.data);
-            const map = new Map();
-            keys.forEach(key => {
-                map.set(key, res.data[key])
+        axios.get("https://tue0305.github.io/recipes.json").then(res => setData(res.data))
+            .catch(err => {
+                console.log(err)
             })
-            setData(Array.from(map))
-        }
-        ).catch(err => {
-            console.log(err)
-        })
     }, [])
-    console.log(data);
+
+    const rederData = () => {
+        const dataRender = []
+        for (let title in data) {
+            dataRender.push(
+                <div>
+                    <h3 style={{ paddingLeft: "25px", marginBottom: "5px" }}>{title}</h3>
+                    <Scroller horizontalScrollbar="hidden">
+                        <div style={{ display: "flex" }}>
+                            {data[title].map((item, index) => {
+                                return <RowListItem key={index} title={item.title} thumbnail={item.thumbnail} longDescription={item.longDescription} />
+                            })}
+                        </div>
+                    </Scroller>
+                </div>
+            )
+        }
+        return dataRender
+    }
     return <>
         <div {...props}>
-            {data && data?.map((element, index) => {
-                const [title, collections] = element;
-                return (
-                    <div>
-                        <h3 style={{paddingLeft:"25px",marginBottom:"5px"}}>{title}</h3>
-                        <Scroller key={index} horizontalScrollbar="hidden">
-                            <div style={{ display: "flex" }}>
-                                {collections.map((item, index) => {
-                                    return <RowListItem key={index} title={item.title} thumbnail={item.thumbnail} longDescription={item.longDescription} />
-                                })}
-                            </div>
-                        </Scroller>
-                    </div>
-                )
-            })}
+            {data && rederData()}
         </div>
     </>
-
-
-}
+})
 export default RowList
