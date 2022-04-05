5abe4f3ceb0a0ae389c24cd41f9cfb05cc392522
andrewbrayn99 <andrewbrayn99@gmail.com>
2022-04-05 08:44:30 +0700

    update topbar

--- src/Components/TopBar/index.jsx
+++ src/Components/TopBar/index.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Topbar() {
   return (
-    <div>Topbar</div>
+    <div style={{ padding: 10, textAlign: 'right', display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+      <img src="https://www.freeiconspng.com/uploads/hd-youtube-logo-png-transparent-background-20.png"
+        style={{ width: 40, height: 40 }} alt="Hd Youtube Logo Png Transparent Background" />&nbsp;
+      Youtube
+    </div>
   )
 }
 
