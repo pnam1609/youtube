3d0e4c590597ca1e758086596858aac0b1cce290
andrewbrayn99 <andrewbrayn99@gmail.com>
2022-04-06 11:31:05 +0700

    upadate routing

--- src/Components/SideBar/index.jsx
+++ src/Components/SideBar/index.jsx
@@ -2,59 +2,43 @@ import React from 'react'
 import { Item } from '@enact/sandstone/Item'
 import { Row, Cell, Column } from '@enact/ui/Layout'
 import Icon from '@enact/sandstone/Icon';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import styles from './index.module.css'
 
 
 
 function SideBar(props) {
+    const navigate = useNavigate()
     return (
         <>
-            <Row style={{ height: '100%', padding: 0, margin: 0,  width: props.sideBarWidth }}>
+            <Row style={{ height: '100%', padding: 0, margin: 0, width: props.sideBarWidth }}>
                 <Column style={{ width: "100%" }}>
                     <div size="25%" style={{ paddingRight: 10, height: "100vh", display: "flex", flexDirection: "column", justifyContent: "space-around" }}>
                         <div>
-                            <Link to='/view/sign-in' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">profile</Icon>Sign in
-                                </Item>
-                            </Link>
-                            <Link to='/view/search' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">search</Icon>Search
-                                </Item>
-                            </Link>
-                            <Link to='/view' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">home</Icon>Home
-                                </Item>
-                            </Link>
-                            <Link to='/view/music' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">music</Icon>Music
-                                </Item>
-                            </Link>
-
-                            <Link to='/view/gaming' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">controller</Icon>Gaming
-                                </Item>
-                            </Link>
-                            <Link to='/view/news' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">bookmark</Icon>News
-                                </Item>
-                            </Link>
-                            <Link to='/view/subscription' className={styles.styleNone}>
-                                <Item className={styles.item}>
-                                    <Icon size="tiny">playspeed</Icon>Subscriptions
-                                </Item>
-                            </Link>
+                            <Item className={styles.item} onClick={() => navigate("/view/sign-in")}>
+                                <Icon size="tiny">profile</Icon>Sign in
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view/search")} >
+                                <Icon size="tiny">search</Icon>Search
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view")}>
+                                <Icon size="tiny">home</Icon>Home
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view/music")}>
+                                <Icon size="tiny">music</Icon>Music
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view/gaming")} >
+                                <Icon size="tiny">controller</Icon>Gaming
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view/news")} >
+                                <Icon size="tiny">bookmark</Icon>News
+                            </Item>
+                            <Item className={styles.item} onClick={() => navigate("/view/subscription")} >
+                                <Icon size="tiny">playspeed</Icon>Subscriptions
+                            </Item>
                         </div>
                         <div>
-                            <Link to='/view/setting' className={styles.styleNone}>
-                                <Item className={styles.item}><Icon size="tiny">gear</Icon>Setting</Item>
-                            </Link>
+                            <Item className={styles.item} onClick={() => navigate("/view/setting")}><Icon size="tiny">gear</Icon>Setting</Item >
                         </div>
                     </div>
                 </Column>
--- src/Views/Setting/index.jsx
+++ src/Views/Setting/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Item from '@enact/sandstone/Item'
 import { Row, Cell } from '@enact/ui/Layout'
 import Scroller from '@enact/sandstone/Scroller'
-import { Link, Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import AutoPlay from './AutoPlay'
 import Preview from './Preview'
 import styles from './index.module.css'
@@ -11,58 +11,58 @@ import Restrict from './Restrict'
 import ResetApp from './ResetApp'
 
 function Setting() {
+    let navigate = useNavigate()
     return (
-
         <Row style={{ height: '100%', padding: 0, marginLeft: 0 }}>
             <Cell size="18%">
                 <Scroller style={{ width: 340, borderRight: 'solid 1px #73706a' }} >
                     <h4>Setting</h4>
                     <Link to="auto-play" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/auto-play")}>
                             Auto Play
                         </Item>
                     </Link>
 
                     <Link to="preview" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/preview")}>
                             Preview with sound
                         </Item>
                     </Link>
 
                     <Link to="restrict" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/restrict")}>
                             Restriced mode
                         </Item>
                     </Link>
 
                     <p style={{ fontSize: 22, borderBottom: 'solid 1px #73706a', color: "#73706a" }}> Link TV & Phone</p>
                     <Link to="wifi" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/wifi")}>
                             Link with wifi
                         </Item>
                     </Link>
 
                     <Link to="code" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/code")}>
                             Link with TV code
                         </Item>
                     </Link>
 
                     <Link to="device" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/device")}>
                             Linked Devices
                         </Item>
                     </Link>
 
                     <p style={{ fontSize: 22, borderBottom: 'solid 1px #73706a', color: "#73706a" }}> LANGUAGES & LOCATION</p>
                     <Link to="language" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/language")}>
                             Languages
                         </Item>
                     </Link>
 
                     <Link to="location" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/location")}>
                             Location
                         </Item>
                     </Link>
@@ -70,25 +70,25 @@ function Setting() {
                     <p style={{ fontSize: 22, borderBottom: 'solid 1px #73706a', color: "#73706a" }}> HISTORY & DATA</p>
 
                     <Link to="clear-watch" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/clear-watch")}>
                             Clear watch history
                         </Item>
                     </Link>
 
                     <Link to="clear-search" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/clear-search")}>
                             Clear search history
                         </Item>
                     </Link>
 
                     <Link to="resetapp" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/resetapp")}>
                             Reset app
                         </Item>
                     </Link>
 
                     <Link to="improve-youtube" className={styles.styleNone}>
-                        <Item style={{ fontSize: 20, height: 60 }}>
+                        <Item style={{ fontSize: 20, height: 60 }} onClick={() => navigate("/view/setting/improve-youtube")}>
                             Improve Youtbe
                         </Item>
                     </Link>
